d338a0494a3b496fd95da1ca7aabddbba31e3458
neffertitijf <jocelynneff@DN0a22b3ad.SUNet>
2014-10-29 15:32:19 -0700

    token uniqueness

--- project5/vw_format.py
+++ project5/vw_format.py
@@ -2,21 +2,27 @@
 import sys
 import json
 
+from nltk.corpus import stopwords
+from itertools import groupby
 
 def vw_format():
+    stop = stopwords.words('english')
+    stop = stop.union(set(["|", ":", "\n"))
+
     for line in sys.stdin:
         obj = json.dumps(line)
         output = ''
         if obj['news_desk'] == 'Sports':
-            output = '1 1.0 '
+            output = '1 1.0 |'
         else:
-            output = '-1 1.0 '
+            output = '-1 1.0 |'
 
         snippet = obj['snippet']
-        lead_paragraph = obj['lead_paragraph']
-        abstract = obj['abstract']
+        tokens = [i for i in snippet.lower().split() if i not in stop]
+        output += "TokenCounts "
+        for key, group in groupby(tokens):
+            output += key + ":" + group + " "
         
-
 
 if __name__ == "__main__":
     vw_format()
