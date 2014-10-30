218ea0fefa13a305933f491dd6050295116559fb
neffertitijf <jocelynneff@rescomp-14-291123.stanford.edu>
2014-10-29 19:52:40 -0700

    doc fix

--- project5/vw_format.py
+++ project5/vw_format.py
@@ -7,21 +7,23 @@ from itertools import groupby
 
 def vw_format():
     stop = stopwords.words('english')
-    stop = stop.union(set(["|", ":", "\n"))
+    stop = stop.union(set(["|", ":", "\n"]))
 
     for line in sys.stdin:
         obj = json.dumps(line)
-        output = ''
-        if obj['news_desk'] == 'Sports':
-            output = '1 1.0 |'
-        else:
-            output = '-1 1.0 |'
+        for article in obj['docs']:
+            output = ''
+            if obj['news_desk'] == 'Sports':
+                output = '1 1.0 |'
+            else:
+                output = '-1 1.0 |'
 
-        snippet = obj['snippet']
-        tokens = [i for i in snippet.lower().split() if i not in stop]
-        output += "TokenCounts "
-        for key, group in groupby(tokens):
-            output += key + ":" + group + " "
+            snippet = obj['snippet']
+            tokens = [i for i in snippet.lower().split() if i not in stop]
+            output += "TokenCounts "
+            for key, group in groupby(tokens):
+                output += key + ":" + group + " "
+            print output
         
 
 if __name__ == "__main__":
