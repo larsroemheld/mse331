241344fa8ebf6119f7f9d07a25e265507bb9154f
neffertitijf <jocelynneff@DN0a2231a6.SUNet>
2014-10-30 16:44:42 -0700

    yay

--- project5/vw_format.py
+++ project5/vw_format.py
@@ -31,10 +31,10 @@ def vw_format():
                 output += key + ":" + str(len(list(group))) + " "
             
             output += "|Headline "
-            output += "wc " + str(len(article['headline']['main'].split())) + " "
+            output += "wc:" + str(len(article['headline']['main'].split())) + " "
 
             output += "|Multimedia "
-            output += "count " + str(len(article['multimedia'].length())) + " "
+            output += "count:" + str(len(article['multimedia'].length())) + " "
 
             print output.encode('utf-8')
         
